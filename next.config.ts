40237ccf77a4cb3d33ab248900a92d8f2e1c75ad
Rafal Sulkowski <rafis2569@gmail.com>
2026-03-23 13:34:57 +0100

    fix: allow SVG in Next.js Image component

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
       canvas: "./empty-module.ts",
     },
   },
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 export default nextConfig;
